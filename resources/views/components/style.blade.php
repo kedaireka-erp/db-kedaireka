196fd6c4adb3eeb088c7955c5ee0e92627d72e1a
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-05 00:26:39 +0700

    Update style.blade.php

--- resources/views/components/style.blade.php
+++ resources/views/components/style.blade.php
@@ -2,7 +2,7 @@
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
     rel="stylesheet" />
 <!-- CSS -->
-<link rel="stylesheet" type="text/css" href="/vendors/styles/core.css" />
+<!-- <link rel="stylesheet" type="text/css" href="/vendors/styles/core.css" /> -->
 <link rel="stylesheet" type="text/css" href="/vendors/styles/icon-font.min.css" />
 <link rel="stylesheet" type="text/css" href="/vendors/styles/style.css" />
     
