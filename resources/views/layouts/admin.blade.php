de246c27805d504afb72ecef69a7794835b5bd3f
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-05 00:17:17 +0700

    asdas

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -14,18 +14,7 @@
 </head>
 
 <body>
-    <div class="pre-loader">
-        <div class="pre-loader-box">
-            <div class="loader-logo">
-                <img src="https://allureindustries.com/files/uploads/2016/03/600.png" alt="" />
-            </div>
-            <div class="loader-progress" id="progress_div">
-                <div class="bar" id="bar1"></div>
-            </div>
-            <div class="percent" id="percent1">0%</div>
-            <div class="loading-text">Loading...</div>
-        </div>
-    </div>
+
     <!-- navbar -->
     <x-navbar></x-navbar>
 
