41674e5a1d071e24ef008c809b1aed6b39eec261
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-04 20:37:07 +0700

    login

---
+++ resources/views/components/style.blade.php
@@ -0,0 +1,14 @@
+<!-- Site favicon -->
+<link rel="apple-touch-icon" sizes="180x180" href="/vendors/images/600.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/vendors/images/600.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/vendors/images/600.png" />
+
+
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap"
+    rel="stylesheet" />
+<!-- CSS -->
+<link rel="stylesheet" type="text/css" href="/vendors/styles/core.css" />
+<link rel="stylesheet" type="text/css" href="/vendors/styles/icon-font.min.css" />
+<link rel="stylesheet" type="text/css" href="/vendors/styles/style.css" />
+    
