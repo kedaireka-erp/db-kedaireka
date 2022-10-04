1022bd0c85df674ef6fd7891e8eb2c7d60ec89b2
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-04 22:58:54 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,7 +7,7 @@
     <!-- Mobile Specific Metas -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="icon">
-    <title>Allure Industries - CRM - {{ $title }}</title>
+    <title>Allure Industries/title>
     @stack('style')
     <x-style></x-style>
 
