7a223be99c7f340a2e54d1c90987ae9dfe086b29
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-05 00:43:27 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -13,11 +13,7 @@
                     <span class="user-name">{{auth()->user()->name}}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-                    {{-- <a class="dropdown-item" href="{{ route('login') }}"><i class="dw dw-logout"></i>Log Out</a> --}}
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                        @csrf
-                        <button type="submit" class="dropdown-item"><i class="dw dw-logout"></i>Log Out</button>
-                    </form>
+                    <a class="dropdown-item" href="/logout"><i class="dw dw-logout"></i>Log Out</a>
                 </div>
             </div>
         </div>
