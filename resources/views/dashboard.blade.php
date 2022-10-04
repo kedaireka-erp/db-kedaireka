affe467ff526ff6d53c85840e2f9c4fbdb571290
Achmat Sodikkun <75171774+sodkun@users.noreply.github.com>
2022-10-04 22:55:57 +0700

    dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -23,7 +23,98 @@
         <input type="hidden" name="user_id" value="{{base64_encode(auth()->user()->id)}}">
         <button type="submit"> Manufacturing</button>
     </form>
+    <form action="http://purchasing.alluresystem.site/login" method="post">
+        <input type="hidden" name="user_id" value="{{base64_encode(auth()->user()->id)}}">
+        <button type="submit"> Purchasing</button>
+    </form>
     <a href="/logout">logout</a>
 
 </body>
-</html>
+</html> -->
+@extends('layouts.admin')
+
+@section('content')
+<div class="main-container">
+    <div class="xs-pd-20-10 pd-ltr-20">
+        <div class="row">
+            <div class="col-12 mb-20">
+                <div class="row">
+                    <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+                        <div class="card-box height-100-p widget-style3">
+                            <div class="d-flex flex-wrap">
+                                <div class="widget-data">
+                                    <div class="weight-700 font-24 text-dark">Sales</div>
+                                </div>
+                                <div class="widget-icon bg-warning">
+                                    <div class="icon">
+                                        <span class="icon-copy bi bi-currency-exchange"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+                        <div class="card-box height-100-p widget-style3">
+                            <div class="d-flex flex-wrap">
+                                <div class="widget-data">
+                                    <div class="weight-700 font-24 text-dark">Purchasing</div>
+                                </div>
+                                <div class="widget-icon bg-danger">
+                                    <div class="icon">
+                                        <span class="icon-copy bi bi-cart4"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+                        <div class="card-box height-100-p widget-style3">
+                            <div class="d-flex flex-wrap">
+                                <div class="widget-data">
+                                    <div class="weight-700 font-24 text-dark">Finance</div>
+                                </div>
+                                <div class="widget-icon bg-success">
+                                    <div class="icon">
+                                        <i class="icon-copy bi bi-currency-dollar"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+                        <div class="card-box height-100-p widget-style3">
+                            <div class="d-flex flex-wrap">
+                                <div class="widget-data">
+                                    <div class="weight-700 font-24 text-dark">Manufacturing</div>
+                                </div>
+                                <div class="widget-icon bg-primary">
+                                    <div class="icon">
+                                        <i class="bi bi-gear-fill" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-6 mb-20">
+                    <form action="http://crm.alluresystem.site/login" method="post">
+                        <input type="hidden" name="user_id" value="{{base64_encode(auth()->user()->id)}}">
+                        <button type="submit" class="card-box height-100-p widget-style3">
+                            <div class="d-flex flex-wrap">
+                                <div class="widget-data">
+                                    <div class="weight-700 font-24 text-dark">CRM</div>
+                                </div>
+                                <div class="widget-icon bg-primary">
+                                    <div class="icon">
+                                        <i class="bi bi-door-open-fill" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic Page Info -->
+    <meta charset="utf-8" />
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="icon">
+    <title>Allure Industries - CRM - {{ $title }}</title>
+    @stack('style')
+    <x-style></x-style>
+
+    <!-- End Google Tag Manager -->
+</head>
+
+<body>
+    <div class="pre-loader">
+        <div class="pre-loader-box">
+            <div class="loader-logo">
+                <img src="https://allureindustries.com/files/uploads/2016/03/600.png" alt="" />
+            </div>
+            <div class="loader-progress" id="progress_div">
+                <div class="bar" id="bar1"></div>
+            </div>
+            <div class="percent" id="percent1">0%</div>
+            <div class="loading-text">Loading...</div>
+        </div>
+    </div>
+    <!-- navbar -->
+    <x-navbar></x-navbar>
+
+    <!-- sidebar -->
+    <!-- <x-sidebar>{{ $title }}</x-sidebar> -->
+
+    <!-- content -->
+    @yield('content')
+
+    <!-- footer -->
+    {{-- <x-footer></x-footer> --}}
+    <!-- welcome modal end -->
+    <!-- js -->
+    <x-script></x-script>
+    @stack('script')
+</body>
+
+</html>
