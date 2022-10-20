aed0425d7b6863d73239157a6abc91c104761b97
Ridwan Ridlo <82161307+waniAEbro@users.noreply.github.com>
2022-10-20 17:38:27 +0700

    Update LoginController.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-        return redirect("/");
+        return view("login");
     }
 
     public function login(Request $request)
